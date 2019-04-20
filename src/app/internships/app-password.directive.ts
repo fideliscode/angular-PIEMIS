06d8a9c994d6d159c6a623af5564ed81a279940b
fideliscode <mapendofidelis@gmail.com>
2019-04-20 15:26:39 +0300

    professional registration

--- src/app/internships/app-password.directive.ts
+++ src/app/internships/app-password.directive.ts
@@ -14,17 +14,17 @@ toggle(span: HTMLElement) {
     this._shown = !this._shown;
     if (this._shown) {
       this.el.nativeElement.setAttribute('type', 'text');
-      span.innerHTML = '<button class="btn btn-small btn-success">  Hide </button> ';
+      span.innerHTML = '<button class="btn btn-small btn-success">Hide </button> ';
     } else {
       this.el.nativeElement.setAttribute('type', 'password');
-      span.innerHTML = '<button class="btn btn-small btn-success">  Show</button>';
+      span.innerHTML = '<button class="btn btn-small btn-success">Show</button>';
     }
   }
 
 setup() {
     const parent = this.el.nativeElement.parentNode;
     const span = document.createElement('span');
-    span.innerHTML = '<button class="btn btn-small btn-success" style="background-color:#008d0d;">  Show </button>';
+    span.innerHTML = '<button class="btn btn-small btn-success" style="background-color:#008d0d;">Show</button>';
     span.addEventListener('click', (event) => {
       this.toggle(span);
     });
--- src/app/user.service.ts
+++ src/app/user.service.ts
@@ -1,9 +1,28 @@
 import { Injectable } from '@angular/core';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor() { }
+  constructor(private httpclient: HttpClient) { 
+  }
+
+
+
+
+  regProfessional(fname: string, lname: string, email: string, password: string, phone: number, role: string)
+  {
+    const body = JSON.stringify({fname, lname, email, password, phone, role});
+    console.log(body);
+    
+    return this.httpclient.post('http://127.0.0.1:3000/users', body, 
+      {headers :new HttpHeaders({'Content-Type': 'application/json',  'X-Requested-With': 'XMLHttpRequest'})
+    })
+  }
+
 }
+
+
